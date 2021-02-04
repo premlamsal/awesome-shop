4696489ab3255b0360cb1f4e6e9ef897322711a4
premlamsal2@gmail.com <premlamsal2@gmail.com>
2021-02-04 18:47:05 +0545

    update

--- source/src/views/MyBooks.vue
+++ source/src/views/MyBooks.vue
@@ -2,10 +2,17 @@
     <div class="content mt-2">
       <b-container>
       <h5>My Books</h5>
-
         <b-row>
           <b-col>
-            <product :products="products"></product>
+          <div v-if="products.length>0">
+            <product :products="products" ></product>
+            </div>
+            <div v-else>
+              <button class="btn btn-success mt-3">
+                Add Book
+              </button>
+              <p class="mt-3">You have no Books. Please add one to start earning.</p>
+            </div>
           </b-col>
         </b-row>
       </b-container>
@@ -34,9 +41,8 @@ export default {
       this.$http
         .get("https://eshop.test/api/loadMyBooks")
         .then((response) => {
-          console.log(response.data);
-          this.products=response.data.data[0].products;
-          console.log(this.product)
+          // console.log(response.data.data[0].products);
+          this.products = response.data.data[0].products
         })
         .catch((error) => {
           console.log(error);
