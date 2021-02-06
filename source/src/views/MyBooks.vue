425ec5ab671d0c3423627a1f48df3ed6532225c1
premlamsal2@gmail.com <premlamsal2@gmail.com>
2021-02-06 20:12:58 +0545

    update

--- source/src/views/MyBooks.vue
+++ source/src/views/MyBooks.vue
@@ -5,17 +5,76 @@
       <h5>My Books</h5>
         <b-row>
           <b-col>
-           <button class="btn btn-success mt-3" @click="$bvModal.show('bv-modal-addbook')">
+           <button class="btn btn-success mt-3" @click="showModal()">
                   Upload Book
                 </button>
           <div v-if="products.length>0">
-            <product :products="products" ></product>
-            </div>
+           
+           <div class="card mt-4">
+           
+           <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Category</th>
+                    <th scope="col">Image</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Slug</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Quantity</th>
+                    <th scope="col">Unit</th>
+                    <th scope="col">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr
+                    v-for="product in products"
+                    v-bind:key="product.id">
+                    <td>
+                       <div v-for="category in categories" v-bind:key="category.id">
+                        <span v-if="product.category_id===category.id">
+                        {{category.name}}
+                        </span>
+                      </div>
+                    
+                    </td>
+                    <td>
+                     <img :src="image" class="img-thumbnail product-image-inside-table" :alt="product.name"  v-for="image in product.image" v-bind:key="image">
+                    </td>
+                    <td>{{ product.name }}</td>
+                    <td>{{ product.slug }}</td>
+                    <td>Rs. {{product.price}}</td>
+                    <td>{{product.quantity}}</td>
+                    <td>
+
+                    <div v-for="unit in units" v-bind:key="unit.id">
+                        <span v-if="product.unit_id===unit.id">
+                        {{unit.short_name}}
+                        </span>
+                      </div>
+                    
+                    
+                    </td>
+                    <td>
+                      <button class="btn btn-warning m-2" @click="loadBookDetails(product.slug)"> <b-icon icon="gear" font-scale="1.2"></b-icon></button> 
+                      <button class="btn btn-danger m-2"> <b-icon icon="x" font-scale="1.2"></b-icon></button>   
+                    </td>
+
+                  </tr>
+                </tbody>
+              </table>
+
+        </div>
+
+
+          </div>
             <div v-else>
              <p class="mt-3">You have no Books. Please add one to start earning.</p>
             </div>
                
-                <b-modal id="bv-modal-addbook" hide-footer title="Add Book" style="width:20em;">
+                <b-modal id="bv-modal-addbook" hide-footer style="width:20em;">
+                  <template v-slot:modal-title>
+                  <span class="text-primary">{{book_title}}</span>
+                </template>
                 <div class="d-block text-center">
                     <div class="row">
                         <div class="col-md-4">
@@ -50,6 +109,17 @@
                         <div class="col-md-7">
                           <input type="text" class="form-control" v-model="book.price"/>
                       <span v-if="errors.price" :class="['errorText']">{{ errors.price[0] }}</span>
+
+                        </div>
+                    </div>
+
+                    <div class="row mt-1">
+                        <div class="col-md-4">
+                          <label>Quantity</label>
+                        </div>
+                        <div class="col-md-7">
+                          <input type="text" class="form-control" v-model="book.quantity"/>
+                      <span v-if="errors.quantity" :class="['errorText']">{{ errors.quantity[0] }}</span>
 
                         </div>
                     </div>
@@ -118,7 +188,9 @@
                     </div>
 
                   </div>
-                <b-button class="mt-3" block @click="uploadBook">Add Book</b-button>
+                <b-button class="mt-3" block @click="editBook" v-if="book_action==='edit'">Edit Book</b-button>
+
+                <b-button class="mt-3" block @click="uploadBook" v-if="book_action==='add'">Add Book</b-button>
               </b-modal>
 
              
@@ -130,11 +202,10 @@
 </template>
 
 <script>
-import Product from "../components/Product";
 export default {
   name: "MyBooks",
   components: {
-    Product,
+ 
   },
   data() {
     return {
@@ -147,7 +218,12 @@ export default {
         more_info:'',
         description:'',
         unit:'',
+        quantity:'',
       },
+      
+      book_title:'Upload Book',
+      book_action:'add',//add or edit
+
       errors:[],
       categories:{},
       units:{},
@@ -161,6 +237,21 @@ export default {
   },
   computed: {},
   methods: {
+     clearBook(){
+      this.book.category='';
+      this.book.name='';
+      this.book.price='';
+      this.book.discount='';
+      this.book.more_info='';
+      this.book.description='';
+      this.book.unit='';
+      this.book.quantity='';
+      this.book.book_id='';
+    },
+    showModal(){
+      this.clearBook();
+      this.$bvModal.show('bv-modal-addbook')
+    },
     loadMyBooks() {
       this.$http
         .get("https://eshop.test/api/loadMyBooks")
@@ -170,7 +261,37 @@ export default {
         .catch((error) => {
           console.log(error);
         });
-    }, //end of loadMyBooks
+    },
+    
+    loadBookDetails(slug){
+      this.book_action="edit";
+      this.book_title="Change Book Information";
+      if(this.book_action==="edit"){
+        this.clearBook();
+            this.$http
+            .get("https://eshop.test/api/productDFS/"+slug)
+            .then((response)=>{
+              const product=response.data.data[0];
+                  this.book.category=product.category_id;
+                  this.book.name=product.name;
+                  this.book.price=product.price;
+                  this.book.discount=product.discount;
+                  this.book.more_info=product.more_info;
+                  this.book.description=product.description;
+                  this.book.unit=product.unit_id;
+                  this.book.quantity=product.quantity;
+                  this.book.id=product.id;
+                  this.$bvModal.show('bv-modal-addbook');
+                  console.log(product);
+            })
+            .catch((response)=>{
+              this.clearBook();
+              console.log(response);
+            })
+      }
+    },
+    
+     //end of loadMyBooks
     handleFileUploads(){
       this.uploadedFiles=this.$refs.files.files;
       console.log(this.uploadedFiles);
@@ -202,17 +323,58 @@ export default {
         console.log(error);
       })
     },
-    clearBook(){
-      this.category='';
-      this.name='';
-      this.price='';
-      this.discount='';
-      this.more_info='';
-      this.description='';
-      this.unit='';
-    },
+   
+    editBook(){
+         this.book_action="Edit Book";
+         this.book_action="edit";
+         let formData = new FormData();
+        
+        formData.append("uploadedFiles",this.uploadedFiles);
 
+        for( var i = 0; i < this.uploadedFiles.length; i++ ){
+            let file = this.uploadedFiles[i];
+
+            formData.append('files[' + i + ']', file);
+        }
+        formData.append("category",this.book.category);
+        formData.append("unit",this.book.unit);
+        formData.append("name",this.book.name);
+        formData.append("quantity",this.book.quantity);
+        formData.append("price",this.book.discount);
+        formData.append("discount",this.book.discount);
+        formData.append("description",this.book.description);
+        formData.append("more_info",this.book.more_info);
+        formData.append("book_id",this.book.id);
+
+
+        this.$http.post("https://eshop.test/api/editBook",formData,{
+          headers: {
+              'Content-Type': 'multipart/form-data'
+          }
+        })
+        .then((response)=>{
+          this.$toast.success(response.data.msg, {
+                timeout: 8000
+            });
+          this.clearBook();
+          this.loadMyBooks();
+          this.$bvModal.hide('bv-modal-addbook')
+        })
+        .catch((error)=>{
+           this.$toast.error(error.response.data.message, {
+                timeout: 4000
+            });
+          this.erros=error.response.data;
+          this.clearBook();
+        });
+
+
+      
+    },
     uploadBook(){
+      this.book_action="add";
+      this.book_title="Upload Book"
+      this.clearBook();
       let formData = new FormData();
 
         
@@ -226,10 +388,12 @@ export default {
         formData.append("category",this.book.category);
         formData.append("unit",this.book.unit);
         formData.append("name",this.book.name);
+        formData.append("quantity",this.book.quantity);
         formData.append("price",this.book.discount);
         formData.append("discount",this.book.discount);
         formData.append("description",this.book.description);
         formData.append("more_info",this.book.more_info);
+        
 
         this.$http.post("https://eshop.test/api/uploadBook",formData,{
           headers: {
@@ -237,13 +401,18 @@ export default {
           }
         })
         .then((response)=>{
+          this.$toast.success(response.data.msg, {
+                timeout: 8000
+            });
           console.log(response);
           this.$bvModal.hide('bv-modal-addbook')
           this.clearBook();
           this.loadMyBooks();
         })
         .catch((error)=>{
-          console.log(error);
+           this.$toast.error(error.response.data.message, {
+                timeout: 4000
+            });
           this.erros=error.response.data;
           this.clearBook();
         });
@@ -256,5 +425,11 @@ export default {
 .content {
   background: white;
   padding: 5em;
+}
+.product-image-inside-table{
+    width: 100px;
+    height: 100px;
+    border-radius: 64px;
+    margin:2px;
 }
 </style>
