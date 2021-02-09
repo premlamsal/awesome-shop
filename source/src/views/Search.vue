283bfc290c6d4f01ab1b1fcd5a0649ad578c22f5
premlamsal2@gmail.com <premlamsal2@gmail.com>
2021-02-09 21:38:39 +0545

    update

--- source/src/views/Search.vue
+++ source/src/views/Search.vue
@@ -8,7 +8,7 @@
             <h4>We found this for you !</h4>
             <div class="product-panel mt-5">
               <div class="product-panel-insider">
-                <product :products="products"></product>
+                <product :products="products.data"></product>
               </div>
               <div class="inner-btn mb-4">
                 <b-button
